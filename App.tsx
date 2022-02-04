1b8d476d2269bfaaea05b48ec8b730f44e54d907
Douglas Santiago <dougsrodrigues@outlook.com>
2022-02-03 21:38:24 -0300

    feat: configure roboto slab font

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  RobotoSlab_400Regular,
+  RobotoSlab_500Medium,
+  RobotoSlab_300Light,
+} from '@expo-google-fonts/roboto-slab';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    RobotoSlab_400Regular,
+    RobotoSlab_500Medium,
+    RobotoSlab_300Light,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
