449fdcb5097d9fa6254699bfe1647add2fad2556
Douglas Santiago <dougsrodrigues@outlook.com>
2022-02-04 21:21:20 -0300

    feat: init create StyledText atom

---
+++ src/app/App.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  useFonts,
+  RobotoSlab_300Light,
+  RobotoSlab_500Medium,
+  RobotoSlab_400Regular,
+  RobotoSlab_600SemiBold,
+} from '@expo-google-fonts/roboto-slab';
+import AppLoading from 'expo-app-loading';
+import styled, { ThemeProvider } from 'styled-components/native';
+import { useColorScheme } from 'react-native';
+
+import themes from '../styles';
+import { StyledText } from '../components/atoms/typography/StyledText';
+
+export default function App() {
+  const deviceTheme = useColorScheme();
+  const { typography } = themes;
+  const theme = themes[deviceTheme] || themes.dark;
+
+  const [fontsLoaded] = useFonts({
+    RobotoSlab_400Regular,
+    RobotoSlab_500Medium,
+    RobotoSlab_300Light,
+    RobotoSlab_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={{ ...typography, ...theme }}>
+      <Container>
+        <StyledText>Olá</StyledText>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/components/atoms/typography/StyledText.tsx
@@ -0,0 +1,41 @@
+import React, { ComponentProps } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { useTheme } from 'styled-components';
+
+interface StyledTextProps {
+  family?: 'regular' | 'medium' | 'light' | 'semiBold';
+  size?: 'small' | 'medium' | 'large';
+  lineHeight?: 'small' | 'medium' | 'large';
+  color?: string | undefined;
+}
+
+type Props = ComponentProps<typeof Text> & StyledTextProps;
+
+function StyledText({
+  family = 'regular',
+  size = 'medium',
+  lineHeight,
+  color,
+  style,
+  ...rest
+}: Props) {
+  const theme = useTheme();
+  const fontFamily = theme.fonts.families[family];
+  const fontSize = theme.fonts.sizes[size];
+
+  const height = lineHeight ? theme.fonts.lineHeight?.[lineHeight] : fontSize;
+
+  const flattenedStyles = StyleSheet.flatten(style);
+
+  return (
+    <Text
+      {...rest}
+      style={[
+        { fontFamily, fontSize, lineHeight: height, color },
+        flattenedStyles,
+      ]}
+    />
+  );
+}
+
+export { StyledText };
