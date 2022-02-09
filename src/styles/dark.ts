a64894bbd026ff4ceb6b39faa15b8233aca3de65
Douglas Santiago <dougsrodrigues@outlook.com>
2022-02-09 16:52:22 -0300

    feat: add new color to styles

--- src/styles/dark.ts
+++ src/styles/dark.ts
@@ -1,6 +1,7 @@
 export default {
   colors: {
     bg: '#060D25',
+    secondary: '#2C303F',
     ice: '#A7B4E0',
     text_primary: '#FFF',
     text_secondary: '#000',
--- src/styles/light.ts
+++ src/styles/light.ts
@@ -1,6 +1,7 @@
 export default {
   colors: {
     bg: '#828CAE',
+    secondary: '#7882A7',
     ice: '#A7B4E0',
     text_primary: '#FFF',
     text_secondary: '#000',
